e2ba026e50c0aa32f631a647e3a404fe99871163
RichardKaranuMbuti <officialforrichardk@gmail.com>
2023-07-01 18:05:50 +0300

    registered ApiKey model in admin

--- companies/admin.py
+++ companies/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from companies.models import Company, CSVFile
+from companies.models import Company, CSVFile, ApiKey
 
 admin.site.register(Company)
 admin.site.register(CSVFile)
+admin.site.register(ApiKey)
